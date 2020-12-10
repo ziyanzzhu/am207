cd080b1d632580c19e193d0e1350d5735a32df09
Ziyan "Zoe" Zhu <zzy950709@gmail.com>
2020-12-10 14:45:33 -0500

    Add files via upload

---
+++ uncertainty.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  8 14:03:53 2020
+
+@author: zoe
+"""
+
+from autograd import numpy as np
+from autograd import grad, elementwise_grad
+from autograd.misc.optimizers import adam, sgd
+from sklearn.datasets import make_moons
+import numpy.random as npr
+import pandas as pd
+import numpy 
+import scipy as sp
+import matplotlib.pyplot as plt
+
+def myentropy(nn_model, weightlist, xdata):
+    '''
+    Usage: for NN_Dropout, use the same weights, duplicated N times
+    for MFVI, pass the sampled weights
+    '''
+
+    #assert xdata.shape[0]==2
+    n_samples = xdata.shape[1]
+    p1narray = np.zeros((len(weightlist), n_samples))
+    if type(nn_model) != list: 
+        for i, w in enumerate(weightlist):
+#             print(w.shape)
+            w = np.reshape(w, (1, nn_model.D))
+            p1narray[i, :] = nn_model.forward(w, xdata) #assumes that the 'model.forward' is dropout-like and has generates different outputs for each i
+    elif type(nn_model) == list: # deterministic 
+        for i, nn in enumerate(nn_model): 
+
+            p1narray[i, :] = nn.forward(weightlist[i], xdata)
+    #print (p_here.shape)
+    p2narray = 1 - p1narray
+    p1narray = np.mean(p1narray, axis=0)
+    p2narray = np.mean(p2narray, axis=0)
+    Hpredcheck = -p1narray*np.log(p1narray) - p2narray*np.log(p2narray)
+    return p1narray, Hpredcheck
+
+# calculate the accuracy for MC dropout
+def auc_calc(x_test, y_test, nn, N, perc, model, weightlist=None): 
+    ''' 
+    Options for model "mc", "bbvi", "ensemble", "deterministic"
+    For BBVI, pass a list of weights. 
+    '''
+    p = []
+    n_test = len(y_test)
+    if model != "deterministic": 
+        if model == "mc":
+            p_mean, entropymean = myentropy(nn, [nn.weights]*N, x_test.T)
+        elif (model == "bbvi") and weightlist is not None: 
+            p_mean, entropymean = myentropy(nn, weightlist, x_test.T)
+        elif model == "ensemble": # deterministic 
+            nn_weights = [] 
+            for nn_here in nn: 
+                nn_weights.append(nn_here.weights) 
+            p_mean, entropymean = myentropy(nn, nn_weights, x_test.T)
+
+        idx = np.argsort(entropymean)
+        y_test = y_test[idx]
+        p_mean = p_mean[idx]
+        y_pred_retained = p_mean[0:int(perc*n_test)] # choosing samples with smallest entropy to evaluate 
+        y_test_retained = y_test[0:int(perc*n_test)]
+        predict_proba = np.round(y_pred_retained)
+  
+        auc = len(y_test_retained[predict_proba==y_test_retained]) / len(y_pred_retained) * 100
+    else: 
+        auc = auc_calc_proba(x_test, y_test, nn, N, perc)
+        
+    return auc
+
+
+# calculate the accuracy for deterministic model
+def auc_calc_proba(x_test, y_test, nn, N, perc):
+    n_samples = len(y_test)
+    auc = np.zeros(N)
+    for j in range(N):
+        p_here = nn.forward(nn.weights, x_test.T)[0][0]
+        idx = np.argsort(p_here)
+        i2 = int((1-perc/2)*n_samples)
+        i1 = int(perc*n_samples/2)
+        idx1 = idx[0:i1] # indices predicted to be 0 
+        idx2 = idx[i2:] # indices predicted to be 1 
+        x0 = x_test[idx1]
+        y0 = y_test[idx1]
+        y1 = y_test[idx2]
+        # print(len(y1[y1==1])+len(y0[y0==0]), len(y0)+len(y1), (len(y1[y1==1])+len(y0[y0==0]))/(n_samples*perc))
+        auc[j] = (len(y0[y0==0]) + len(y1[y1==1]))/(len(y0) + len(y1)) * 100
+    return auc
+
+def auc_calc_bbvi(x_test, y_test, 
+
+# NEED TO PLOT THE TRAINING DATA SEPARATELY!
+def plot_entropycontours(x, y, model, weightlist, ax, title, poly_degree=1, test_points=None, shaded=True, interval=np.arange(-6, 6, 0.1)):
+    '''
+    plot_decision_boundary plots the training data and the decision boundary of the classifier.
+    input:
+       x - a numpy array of size N x 2, each row is a patient, each column is a biomarker
+       y - a numpy array of length N, each entry is either 0 (no cancer) or 1 (cancerous)
+       models - an array of classification models
+       ax - axis to plot on
+       poly_degree - the degree of polynomial features used to fit the model
+       test_points - test data
+       shaded - whether or not the two sides of the decision boundary are shaded
+    returns: 
+       ax - the axis with the scatter plot
+    
+    '''
+    # Plot data
+    # ax.scatter(x[y == 1, 0], x[y == 1, 1], alpha=0.2, c='red', label='class 1')
+    # ax.scatter(x[y == 0, 0], x[y == 0, 1], alpha=0.2, c='blue', label='class 0')
+    
+    # Create mesh
+    #interval = np.arange(-6, 6, 0.1)
+    n = np.size(interval)
+    x1, x2 = np.meshgrid(interval, interval)
+    x1 = x1.reshape(-1, 1)
+    x2 = x2.reshape(-1, 1)
+    xx = np.concatenate((x1, x2), axis=1)
+    print ('xx', xx.shape)
+    # Predict on mesh points
+    
+    yy = myentropy(model, weightlist, xx.T)[1] #change this to entropy func  
+    yy = yy.reshape((n, n))
+    
+    cl = plt.imshow(yy, origin='lower', extent=(min(interval), max(interval), min(interval), max(interval)))
+    plt.colorbar()
+    plt.title(title)
+    plt.show()
+    
+    if test_points is not None:
+        for i in range(len(test_points)):
+            pt = test_points[i]
+            if i == 0:
+                ax.scatter(pt[0], pt[1], alpha=1., s=50, color='black', label='test data')
+            else:
+                ax.scatter(pt[0], pt[1], alpha=1., s=50, color='black')
+        
+    ax.set_xlim((-5.5, 5.5))
+    ax.set_ylim((-5.5, 5.5))
+    ax.set_xlabel('x_1')
+    ax.set_ylabel('x_2')
+    ax.legend(loc='best')
+    return ax
+
+if __name__ == '__main__':
+    pass
