910809520df55461dc62c7dbfd7ceb6880e4b9fd
Ziyan <zzy950709@gmail.com>
2020-12-19 17:26:29 +0000

    update toy examples

--- baselines.py
+++ baselines.py
@@ -173,6 +173,172 @@ class Feedforward:
         
 
 
+
+# make sure the dimension is 1 by D 
+class Feedforward_Dropout_Beta:
+    def __init__(self, architecture, random=None, weights=None):
+        self.params = {'H': architecture['width'],
+                       'L': architecture['hidden_layers'],
+                       'D_in': architecture['input_dim'],
+                       'D_out': architecture['output_dim'],
+                       'activation_type': architecture['activation_fn_type'],
+                       'activation_params': architecture['activation_fn_params'],
+                       'dropout_rate': architecture['dropout_rate']} 
+
+        self.D = (  (architecture['input_dim'] * architecture['width'] + architecture['width'])
+                  + (architecture['output_dim'] * architecture['width'] + architecture['output_dim'])
+                  + (architecture['hidden_layers'] - 1) * (architecture['width']**2 + architecture['width'])
+                 )
+
+        if random is not None:
+            self.random = random
+        else:
+            self.random = np.random.RandomState(0)
+
+        self.h = architecture['activation_fn']
+
+        if weights is None:
+            self.weights = self.random.normal(0, 1, size=(1, self.D))
+        else:
+            self.weights = weights
+
+        self.objective_trace = np.empty((1, 1))
+        self.weight_trace = np.empty((1, self.D))
+
+
+    def forward(self, weights, x):
+        ''' Forward pass given weights and input '''
+        H = self.params['H']
+        D_in = self.params['D_in']
+        D_out = self.params['D_out']
+
+        assert weights.shape[1] == self.D
+
+        #Generate random mask
+        num_zero = int(self.D*self.params['dropout_rate'])
+        rng = np.random.default_rng()
+        mask = rng.binomial(n=1, p=1-self.params['dropout_rate'], size=(1,self.D))
+        #mask = np.ones(self.D)
+        #mask[rng.integers(low=0, high=self.D, size=num_zero)] = 0
+        #mask = mask.reshape((1, self.D))
+        unmasked_indices = (self.params['D_out'] * self.params['H'] + self.params['D_out'])
+        mask[0, -unmasked_indices:] =1
+        #print (mask)
+        weights = np.multiply(mask, weights)
+
+        if len(x.shape) == 2:
+            assert x.shape[0] == D_in 
+            x = x.reshape((1, D_in, -1))
+        else:
+            assert x.shape[1] == D_in
+
+        weights = weights.T
+
+        #input to first hidden layer
+        W = weights[:H * D_in].T.reshape((-1, H, D_in))
+        b = weights[H * D_in:H * D_in + H].T.reshape((-1, H, 1))
+        input = self.h(np.matmul(W, x) + b)
+        index = H * D_in + H
+
+        assert input.shape[1] == H
+
+        #additional hidden layers
+        for _ in range(self.params['L'] - 1):
+            before = index
+            W = weights[index:index + H * H].T.reshape((-1, H, H))
+            index += H * H
+            b = weights[index:index + H].T.reshape((-1, H, 1))
+            index += H
+            output = np.matmul(W, input) + b
+            input = self.h(output)
+
+            assert input.shape[1] == H
+
+        #output layer
+        W = weights[index:index + H * D_out].T.reshape((-1, D_out, H))
+        b = weights[index + H * D_out:].T.reshape((-1, D_out, 1))
+        output = 1/(np.exp(-(np.matmul(W, input) + b)) + 1)
+        assert output.shape[1] == self.params['D_out']
+
+        return output
+
+    def make_objective(self, x_train, y_train, reg_param):
+
+        def objective(W, t):
+            #squared_error = np.linalg.norm(y_train - self.forward(W, x_train), axis=1)**2
+            sig = self.forward(W, x_train).flatten()
+            y_train_flat = y_train.flatten()
+            squared_error = -np.dot(y_train_flat[y_train_flat == 1], np.log(sig[y_train_flat == 1])) - np.dot(1-y_train_flat[y_train_flat ==0], np.log(1-sig[y_train_flat==0]))
+
+            if reg_param is None:
+                sum_error = np.sum(squared_error)
+                return sum_error
+            else:
+                mean_error = squared_error + reg_param * np.linalg.norm(W)
+
+                return mean_error
+
+        return objective, grad(objective)
+
+    def fit(self, x_train, y_train, params, reg_param=None):
+
+        assert x_train.shape[0] == self.params['D_in']
+        assert y_train.shape[0] == self.params['D_out']
+
+        ### make objective function for training
+        self.objective, self.gradient = self.make_objective(x_train, y_train, reg_param)
+
+        ### set up optimization
+        step_size = 0.01
+        max_iteration = 5000
+        check_point = 500
+        weights_init = self.weights.reshape((1, -1))
+        mass = None
+        optimizer = 'adam'
+        random_restarts = 5
+
+        if 'step_size' in params.keys():
+            step_size = params['step_size']
+        if 'max_iteration' in params.keys():
+            max_iteration = params['max_iteration']
+        if 'check_point' in params.keys():
+            check_point = params['check_point']
+        if 'init' in params.keys():
+            weights_init = params['init']
+        if 'call_back' in params.keys():
+            call_back = params['call_back']
+        if 'mass' in params.keys():
+            mass = params['mass']
+        if 'optimizer' in params.keys():
+            optimizer = params['optimizer']
+        if 'random_restarts' in params.keys():
+            random_restarts = params['random_restarts']
+
+        def call_back(weights, iteration, g):
+            #Actions per optimization step 
+            objective = self.objective(weights, iteration)
+            self.objective_trace = np.vstack((self.objective_trace, objective))
+            self.weight_trace = np.vstack((self.weight_trace, weights))
+            if iteration % check_point == 0:
+                print("Iteration {} lower bound {}; gradient mag: {}".format(iteration, objective, np.linalg.norm(self.gradient(weights, iteration))))
+
+        ### train with random restarts
+        optimal_obj = 1e16
+        optimal_weights = self.weights
+
+        for i in range(random_restarts):
+            if optimizer == 'adam':
+                adam(self.gradient, weights_init, step_size=step_size, num_iters=max_iteration, callback=call_back)
+            local_opt = np.min(self.objective_trace[-100:])
+            if local_opt < optimal_obj:
+                opt_index = np.argmin(self.objective_trace[-100:])
+                self.weights = self.weight_trace[-100:][opt_index].reshape((1, -1))
+            weights_init = self.random.normal(0, 1, size=(1, self.D))
+
+        self.objective_trace = self.objective_trace[1:]
+        self.weight_trace = self.weight_trace[1:]
+
+
 # make sure the dimension is 1 by D 
 class Feedforward_Dropout:
     def __init__(self, architecture, random=None, weights=None):
@@ -340,168 +506,6 @@ class Feedforward_Dropout:
 
 
 
-# make sure the dimension is 1 by D 
-class Feedforward_Dropout_Beta:
-    def __init__(self, architecture, random=None, weights=None):
-        self.params = {'H': architecture['width'],
-                       'L': architecture['hidden_layers'],
-                       'D_in': architecture['input_dim'],
-                       'D_out': architecture['output_dim'],
-                       'activation_type': architecture['activation_fn_type'],
-                       'activation_params': architecture['activation_fn_params'],
-                       'dropout_rate': architecture['dropout_rate']} 
-
-        self.D = (  (architecture['input_dim'] * architecture['width'] + architecture['width'])
-                  + (architecture['output_dim'] * architecture['width'] + architecture['output_dim'])
-                  + (architecture['hidden_layers'] - 1) * (architecture['width']**2 + architecture['width'])
-                 )
-
-        if random is not None:
-            self.random = random
-        else:
-            self.random = np.random.RandomState(0)
-
-        self.h = architecture['activation_fn']
-
-        if weights is None:
-            self.weights = self.random.normal(0, 1, size=(1, self.D))
-        else:
-            self.weights = weights
-
-        self.objective_trace = np.empty((1, 1))
-        self.weight_trace = np.empty((1, self.D))
-
-
-    def forward(self, weights, x):
-        ''' Forward pass given weights and input '''
-        H = self.params['H']
-        D_in = self.params['D_in']
-        D_out = self.params['D_out']
-
-        assert weights.shape[1] == self.D
-
-        #Generate random mask
-        num_zero = int(self.D*self.params['dropout_rate'])
-        rng = np.random.default_rng()
-        mask = rng.binomial(n=1, p=1-self.params['dropout_rate'], size=(1,self.D))
-        #mask = np.ones(self.D)
-        #mask[rng.integers(low=0, high=self.D, size=num_zero)] = 0
-        #mask = mask.reshape((1, self.D))
-        unmasked_indices = (self.params['D_out'] * self.params['width'] + self.params['D_out'])
-        mask[-unmasked_indices:] =1
-        weights = np.multiply(mask, weights)
-
-        if len(x.shape) == 2:
-            assert x.shape[0] == D_in 
-            x = x.reshape((1, D_in, -1))
-        else:
-            assert x.shape[1] == D_in
-
-        weights = weights.T
-
-        #input to first hidden layer
-        W = weights[:H * D_in].T.reshape((-1, H, D_in))
-        b = weights[H * D_in:H * D_in + H].T.reshape((-1, H, 1))
-        input = self.h(np.matmul(W, x) + b)
-        index = H * D_in + H
-
-        assert input.shape[1] == H
-
-        #additional hidden layers
-        for _ in range(self.params['L'] - 1):
-            before = index
-            W = weights[index:index + H * H].T.reshape((-1, H, H))
-            index += H * H
-            b = weights[index:index + H].T.reshape((-1, H, 1))
-            index += H
-            output = np.matmul(W, input) + b
-            input = self.h(output)
-
-            assert input.shape[1] == H
-
-        #output layer
-        W = weights[index:index + H * D_out].T.reshape((-1, D_out, H))
-        b = weights[index + H * D_out:].T.reshape((-1, D_out, 1))
-        output = 1/(np.exp(-(np.matmul(W, input) + b)) + 1)
-        assert output.shape[1] == self.params['D_out']
-
-        return output
-
-    def make_objective(self, x_train, y_train, reg_param):
-
-        def objective(W, t):
-            #squared_error = np.linalg.norm(y_train - self.forward(W, x_train), axis=1)**2
-            sig = self.forward(W, x_train).flatten()
-            y_train_flat = y_train.flatten()
-            squared_error = -np.dot(y_train_flat[y_train_flat == 1], np.log(sig[y_train_flat == 1])) - np.dot(1-y_train_flat[y_train_flat ==0], np.log(1-sig[y_train_flat==0]))
-
-            if reg_param is None:
-                sum_error = np.sum(squared_error)
-                return sum_error
-            else:
-                mean_error = squared_error + reg_param * np.linalg.norm(W)
-
-                return mean_error
-
-        return objective, grad(objective)
-
-    def fit(self, x_train, y_train, params, reg_param=None):
-
-        assert x_train.shape[0] == self.params['D_in']
-        assert y_train.shape[0] == self.params['D_out']
-
-        ### make objective function for training
-        self.objective, self.gradient = self.make_objective(x_train, y_train, reg_param)
-
-        ### set up optimization
-        step_size = 0.01
-        max_iteration = 5000
-        check_point = 500
-        weights_init = self.weights.reshape((1, -1))
-        mass = None
-        optimizer = 'adam'
-        random_restarts = 5
-
-        if 'step_size' in params.keys():
-            step_size = params['step_size']
-        if 'max_iteration' in params.keys():
-            max_iteration = params['max_iteration']
-        if 'check_point' in params.keys():
-            check_point = params['check_point']
-        if 'init' in params.keys():
-            weights_init = params['init']
-        if 'call_back' in params.keys():
-            call_back = params['call_back']
-        if 'mass' in params.keys():
-            mass = params['mass']
-        if 'optimizer' in params.keys():
-            optimizer = params['optimizer']
-        if 'random_restarts' in params.keys():
-            random_restarts = params['random_restarts']
-
-        def call_back(weights, iteration, g):
-            ''' Actions per optimization step '''
-            objective = self.objective(weights, iteration)
-            self.objective_trace = np.vstack((self.objective_trace, objective))
-            self.weight_trace = np.vstack((self.weight_trace, weights))
-            if iteration % check_point == 0:
-                print("Iteration {} lower bound {}; gradient mag: {}".format(iteration, objective, np.linalg.norm(self.gradient(weights, iteration))))
-
-        ### train with random restarts
-        optimal_obj = 1e16
-        optimal_weights = self.weights
-
-        for i in range(random_restarts):
-            if optimizer == 'adam':
-                adam(self.gradient, weights_init, step_size=step_size, num_iters=max_iteration, callback=call_back)
-            local_opt = np.min(self.objective_trace[-100:])
-            if local_opt < optimal_obj:
-                opt_index = np.argmin(self.objective_trace[-100:])
-                self.weights = self.weight_trace[-100:][opt_index].reshape((1, -1))
-            weights_init = self.random.normal(0, 1, size=(1, self.D))
-
-        self.objective_trace = self.objective_trace[1:]
-        self.weight_trace = self.weight_trace[1:]
 
 
 # Deep ensemble
